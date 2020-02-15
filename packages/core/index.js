e093e62447f5952a0c28e8f2617db704a2ee2fb0
Maurice <maurice.ronet.dominguez@gmail.com>
2020-02-15 22:01:18 +0100

    feat(core): adds utility isInstanceOfIO

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,6 @@
 const { IO, isFunction } = require("crocks");
 const { map, when } = require("ramda");
+const isSameType = require("crocks/core/isSameType");
 
 // doWith :: Monad m => ((* -> a -> * -> m a), {String: (* -> a)}) -> { String: (* -> m a)}
 const doWith = (f, src) => map(when(isFunction, f), src);
@@ -7,7 +8,11 @@ const doWith = (f, src) => map(when(isFunction, f), src);
 // toIO :: (* -> a) -> (* -> IO a)
 const toIO = f => (...args) => IO(() => f(...args));
 
+// isInstanceOfIO :: a -> Boolean
+const isInstanceOfIO = x => isSameType(x, IO);
+
 module.exports = {
   doWith,
-  toIO
+  toIO,
+  isInstanceOfIO
 };
