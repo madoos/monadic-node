bda154eaacd9812dabb3e7b8a7e72f06ca976329
Maurice <maurice.ronet.dominguez@gmail.com>
2020-04-19 20:11:20 +0200

    feat(core): adds natural transformation streamToAsync for Stream ADT

--- packages/core/Stream/nt.js
+++ packages/core/Stream/nt.js
@@ -1,21 +1,52 @@
 const Stream = require("./");
+const { curryN, init, last, is } = require("ramda");
+const { Async } = require("crocks");
+
+const isFunction = is(Function);
+
+const overloadForFunctions = nt => {
+  return curryN(nt.length, (...args) => {
+    const target = last(args);
+    const params = init(args);
+
+    return isFunction(target)
+      ? curryN(target.length, (...args) => {
+          return nt(...params, target(...args));
+        })
+      : nt(...args);
+  });
+};
 
 const asyncToStream = f => (...args) => {
   return Stream(() => {
     const readable = Stream.createReadable();
     const async = f(...args);
     async.fork(
+      e => readable.emit("error", e),
       x => {
         readable.push(x);
         readable.push(null);
-      },
-      e => readable.emit("error", e)
+      }
     );
 
     return readable;
   });
 };
 
+const streamToAsync = stream => {
+  return Async((reject, resolve) => {
+    let last;
+    stream.consume(
+      x => {
+        last = x;
+      },
+      reject,
+      () => resolve(last)
+    );
+  });
+};
+
 module.exports = {
-  asyncToStream
+  asyncToStream,
+  streamToAsync: overloadForFunctions(streamToAsync)
 };
